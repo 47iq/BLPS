1ceb23eec7f4a0f49788369cd9345cdaa45d9d86
Danila Lysenko <DanyPs1@yandex.ru>
2023-04-19 00:26:59 +0300

    Fix wildfly issues

--- src/main/java/org/iq47/ServingWebContentApplication.java
+++ src/main/java/org/iq47/ServingWebContentApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("org.iq47.model")
 @EntityScan("org.iq47.model.entity")
-public class ServingWebContentApplication {
+public class ServingWebContentApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
