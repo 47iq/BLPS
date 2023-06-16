dbe555367a7c18c931f0acfae0eb2f6d2a890a75
Danila Lysenko <DanyPs1@yandex.ru>
2023-06-16 21:59:05 +0300

    camunda wip

--- src/main/java/org/iq47/delegates/LoadTicketsByParameters.java
+++ src/main/java/org/iq47/delegates/LoadTicketsByParameters.java
@@ -3,6 +3,8 @@ package org.iq47.delegates;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.spin.json.SpinJsonNode;
+import org.iq47.model.entity.City;
 import org.iq47.model.entity.Ticket;
 import org.iq47.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import static org.camunda.spin.Spin.JSON;
+
 @Named("loadTicketsByParameters")
 @RequiredArgsConstructor
 public class LoadTicketsByParameters implements JavaDelegate {
 
-    private SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+    private SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
 
     @Autowired
     private TicketService ticketService;
@@ -33,6 +37,13 @@ public class LoadTicketsByParameters implements JavaDelegate {
         //remove paging?
         List<Ticket> tickets = ticketService.findTickets(departureCity, arrivalCity, departueDate, null, zoneId, 1);
 
-        delegateExecution.setVariable("foundTickets", tickets);
+        SpinJsonNode json = JSON("[]");
+        for(Ticket ticket: tickets) {
+            SpinJsonNode elem = JSON("{\"label\": \"" + ticket.getId()+ "\",\"value\": \"" + ticket.getId() + "\"}");
+            json.append(elem);
+        }
+
+        delegateExecution.setVariable("foundTicketsText", tickets.stream().peek(Object::toString));
+        delegateExecution.setVariable("foundTickets", json);
     }
 }
